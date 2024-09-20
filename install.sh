d8770f01e902fb236573dab64160e7b81b6e8876
Daniel <jhonpaco@frba.utn.edu.ar>
2024-09-19 23:29:07 -0300

    :tada:

---
+++ install.sh
@@ -0,0 +1,4 @@
+
+sudo tar -xvzf ~/Downloads/https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz -C /usr/local/bin
+ngrok config add-authtoken 2G4xo9bF8weamzf8IP90qsyvDJY_4B4SkUp9V1nfjfptUArXK
+ngrok http --domain=touching-ghost-sadly.ngrok-free.app 80
